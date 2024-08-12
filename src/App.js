c3682eed4161cd2e84bbd7657437405e5d5f0fbd
lamarrelli <149334072+lamarrelli@users.noreply.github.com>
2024-08-12 13:58:20 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,121 @@
+import React, { useState } from 'react';
+import images from './data/images';
+import './App1.css';
+
+function App() {
+  const [mode, setMode] = useState('menu'); // 'menu', 'quiz', or 'gallery'
+  const [currentImage, setCurrentImage] = useState(getRandomImage());
+  const [userAnswer, setUserAnswer] = useState('');
+  const [feedback, setFeedback] = useState('');
+  const [isImageEnlarged, setIsImageEnlarged] = useState(false);
+
+  function getRandomImage() {
+    return images[Math.floor(Math.random() * images.length)];
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (userAnswer.trim().toLowerCase() === currentImage.correctAnswer.toLowerCase()) {
+      setFeedback('Giusto! Ben fatto.');
+    } else {
+      setFeedback(`Sbagliato, la risposta giusta era: ${currentImage.correctAnswer}`);
+    }
+  };
+
+  const handleNext = () => {
+    setCurrentImage(getRandomImage());
+    setUserAnswer('');
+    setFeedback('');
+  };
+
+  const toggleImageSize = () => {
+    setIsImageEnlarged(!isImageEnlarged);
+  };
+
+  const goBackToMenu = () => {
+    setMode('menu');
+    setUserAnswer('');
+    setFeedback('');
+    setIsImageEnlarged(false);
+  };
+
+  return (
+    <div className="container">
+      {mode === 'menu' && (
+        <div className="menu card">
+          <h1 className="title">Quiz dei vetrini di istologia</h1>
+          <div className="menu-buttons">
+            <button onClick={() => setMode('quiz')} className="menu-button">Inizia Quiz</button>
+            <button onClick={() => setMode('gallery')} className="menu-button">Guarda la galleria</button>
+          </div>
+        </div>
+      )}
+
+      {mode === 'quiz' && (
+        <div className="card">
+          <header className="banner">
+            <button onClick={goBackToMenu} className="home-button">Home</button>
+            <h1 className="banner-title">Istologia Quiz</h1>
+          </header>
+          <img
+            className={`quiz-image ${isImageEnlarged ? 'enlarged' : ''}`}
+            src={currentImage.url}
+            alt="quiz"
+            onClick={toggleImageSize}
+          />
+          <p className="question">{currentImage.question}</p>
+          <form onSubmit={handleSubmit}>
+            <input
+              className="input"
+              type="text"
+              value={userAnswer}
+              onChange={(e) => setUserAnswer(e.target.value)}
+              placeholder="La tua risposta"
+            />
+            <button className="submit-button" type="submit">Cliccami</button>
+          </form>
+          {feedback && (
+            <>
+              <p className={`feedback ${feedback.startsWith('Correct') ? 'correct' : 'incorrect'}`}>
+                {feedback}
+              </p>
+              <button
+                className="next-button"
+                onClick={handleNext}
+              >
+                Prossima immagine
+              </button>
+            </>
+          )}
+        </div>
+      )}
+
+      {mode === 'gallery' && (
+        <div className="card">
+          <header className="banner">
+            <h1 className="banner-title">Galleria dei vetrini</h1>
+          </header>
+          <div className="gallery">
+            {images.map((image, index) => (
+              <div key={index} className="gallery-item">
+                <img
+                  className="gallery-image"
+                  src={image.url}
+                  alt={`gallery-${index}`}
+                  onClick={toggleImageSize}
+                />
+                <p className="gallery-answer">{image.correctAnswer}</p>
+              </div>
+            ))}
+          </div>
+          <button onClick={goBackToMenu} className="menu-button-end">Menù</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
+
+
+
---
+++ src/data/images.js
@@ -0,0 +1,77 @@
+// src/data/images.js
+
+const images = [
+    {
+      url: "https://upload.wikimedia.org/wikipedia/commons/8/83/Basophil.jpg",  // Puoi sostituire con un URL di un'immagine reale
+      question: "Cos'è mostrato nell'immagine?",
+      correctAnswer: "Basofilo"
+    },
+    {
+      url: "https://upload.wikimedia.org/wikipedia/commons/b/b2/Tiroides_03_100X.JPG",
+      question: "Che tipo di cellula è?",
+      correctAnswer: "Parafollicolare"
+    },
+    {
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Spleen_hyaloserositis_-_low_mag.jpg/220px-Spleen_hyaloserositis_-_low_mag.jpg",
+      question: "Quale organo?",
+      correctAnswer: "Milza"
+    },
+
+    {
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Small_intestine_low_mag.jpg/260px-Small_intestine_low_mag.jpg",
+      question: "Cos'è?",
+      correctAnswer: "Villo intestinale"
+    },
+    {
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Cervical_intraepithelial_neoplasia_%281%29_normal_squamous_epithelium.jpg/640px-Cervical_intraepithelial_neoplasia_%281%29_normal_squamous_epithelium.jpg",
+      question: "Che tipo di tessuto è?",
+      correctAnswer: "tessuto epiteliale"
+    },
+    {
+      url: "https://upload.wikimedia.org/wikipedia/commons/0/0c/Compact_bone_-_ground_cross_section.jpg",
+      question: "Che tessuto è? Elenca i metodi istologici di preparazione del vetrino",
+      correctAnswer: "Tessuto osseo"
+    },
+    {
+      url: "https://istologia.unige.it/sites/istologia.unige.it/files/styles/unige_stretto/public/2022-04/Figura%20S17.jpg?itok=0ZAEDVSx",
+      question: "Che tipo di cellula è? Che colorazione è stata applicata?",
+      correctAnswer: "Adipociti, Sudan Black"
+    },
+    {
+      url: "https://www.atlanteistologia.unito.it/photos/cartial20x.jpg",
+      question: "Che cosa rappresenta?",
+      correctAnswer: "Cartilagine"
+    },
+    {
+      url: "https://www.medicinapertutti.it/wp-content/uploads/anatomia_normale/anatomia_microscopica/img_anatomia_microscopica/struttura_del_timo.jpg",
+      question: "Che cosa rappresenta?",
+      correctAnswer: "timo"
+    },
+
+    {
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Solid_cell_nest_of_the_thyroid_gland_-_high_mag.jpg/220px-Solid_cell_nest_of_the_thyroid_gland_-_high_mag.jpg",
+      question: "Che organo rappresenta?",
+      correctAnswer: "tiroide"
+    },
+    {
+      url: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgzAjsozfYM5DghBbVhXYw4Md3CqLpZzN5bByqJZtpicoHtQRDhM4VcIilt6vXQ0cvgjsDcXxNLm33Z7SnN4Dez-UVemwE4slL8oU2VQUKIET0NMSNgMLEaWtIKKsi_O9sPdm28Saz-13g/s1600/Milza+4x.png",
+      question: "Che organo rappresenta?",
+      correctAnswer: "milza"
+    },
+    {
+      url: "https://letteredallafacolta.univpm.it/wp-content/uploads/2018/06/word-image-8.png",
+      question: "Distingui le cellule del sangue",
+      correctAnswer: "eritrociti e neutrofilo"
+    },
+
+    {
+      url: "https://www.biopills.net/wp-content/uploads/2019/06/questa-e1560072666900.jpg",
+      question: "Distingui le cellule del sangue",
+      correctAnswer: "eritrociti e monocita"
+    },
+    // Aggiungi altre immagini e domande
+  ];
+  
+  export default images;
+  
+  
